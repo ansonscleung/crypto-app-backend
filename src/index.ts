e3e8ca72d2259c144e79660dcab06b1770dd22b8
Anson Leung <ansonscleung@gmail.com>
2024-02-21 00:59:52 +0800

    Support upto 2 simutaneous calls for 9 currencies

--- src/index.ts
+++ src/index.ts
@@ -15,39 +15,63 @@ const PORT = process.env.PORT || 5000;
 const COINMARKETCAP_API_DOMAIN = process.env.COINMARKETCAP_API_DOMAIN as string;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY as string;
 
-// demo API call with BTC/ETH/LTC-USD pairs
-app.get("/api/quotes", (req: Request, res: Response) => {
-  fetch(
-    `${COINMARKETCAP_API_DOMAIN}/v2/cryptocurrency/quotes/latest?symbol=btc,eth,ltc&convert=USD`,
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers":
-          "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-        "Content-Type": "application/json",
-        "X-CMC_PRO_API_KEY": COINMARKETCAP_API_KEY,
-      },
-    }
-  )
-    .then((response) => response.json())
-    //TODO: types for CoinMarketCap API results
-    .then(({ data }: { data: { [key: string]: any } }) => {
-      //Assuming API returns >1 results
-      let result = Object.values(data).map((c) => {
-        const {
-          symbol,
-          name,
-          last_updated,
-          quote: {
-            USD: { price, volume_24h: volume, percent_change_1h: change },
-          },
-        } = c[0];
-        return { symbol, name, price, volume, change, last_updated };
-      });
-      res.json(result);
-    })
-    .catch((error) => console.error("Error:", error));
+const coinMarketCapOptions = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    "Content-Type": "application/json",
+    "X-CMC_PRO_API_KEY": COINMARKETCAP_API_KEY,
+  },
+};
+
+const getQuote = async (symbols: Array<String>, fiats: Array<String>) => {
+  const symbolsString = symbols.join(",");
+  const fiatsString = fiats.join(",");
+  return (
+    fetch(
+      `${COINMARKETCAP_API_DOMAIN}/v2/cryptocurrency/quotes/latest?symbol=${symbolsString}&convert=${fiatsString}`,
+      coinMarketCapOptions
+    )
+      .then((response) => response.json())
+      //TODO: types for CoinMarketCap API results
+      .then(({ data }: { data: { [key: string]: any } }) => {
+        let result = Object.values(data).map((quote) => {
+          // Get first quote if >1 quotes
+          const firstQuote = Array.isArray(quote) ? quote[0] : quote;
+          const {
+            symbol,
+            name,
+            last_updated,
+            quote: {
+              USD: { price, volume_24h: volume, percent_change_1h: change },
+            },
+          } = firstQuote;
+          return { symbol, name, price, volume, change, last_updated };
+        });
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        return null;
+      })
+  );
+};
+
+app.get("/api/quotes", async (req: Request, res: Response) => {
+  try {
+    // Split Crpyto to call by 5 according to CMC API
+    const response1 = await getQuote(
+      ["BTC", "ETH", "LTC", "XMR", "XRP"],
+      ["USD"]
+    );
+    const response2 = await getQuote(["DOGE", "DASH", "MAID", "LSK"], ["USD"]);
+    const result = response1 && response2 ? response1.concat(response2) : [];
+    res.json(result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
 app.listen(PORT, () => {
