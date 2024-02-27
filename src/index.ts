7c07f28149fd80b5461018914b54d9401c644fc2
Anson Leung <ansonscleung@gmail.com>
2024-02-28 02:59:51 +0800

    Unit Tests for ports change and abnormal API responses

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/api/quotes", quotesCacheMiddleware, quotesController);
 
-const server = app.listen(PORT, () => {
+export const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
--- src/tests/quotes.test.ts
+++ src/tests/quotes.test.ts
@@ -1,15 +1,23 @@
-import { jest, describe, expect, it, afterEach } from "@jest/globals";
+import {
+  jest,
+  describe,
+  expect,
+  it,
+  afterEach,
+  beforeEach,
+} from "@jest/globals";
 import request from "supertest";
 import server from "../index";
 import { getQuote, quotesController } from "../controllers/quotes.controllers";
-import { createRequest, createResponse} from "node-mocks-http"
+import { createRequest, createResponse } from "node-mocks-http";
+import { Server } from "http";
 
 require("dotenv").config();
 
-const unmockedFetch = global.fetch
+const unmockedFetch = global.fetch;
 
 afterEach(async () => {
-  global.fetch = unmockedFetch
+  global.fetch = unmockedFetch;
   await server.close();
 });
 
@@ -60,6 +68,58 @@ const MOCK_RES = {
   },
 };
 
+const MOCK_400_RES = {
+  status: {
+    timestamp: "2018-06-02T22:51:28.209Z",
+    error_code: 500,
+    error_message: "An internal server error occurred",
+    elapsed: 10,
+    credit_count: 0,
+  },
+};
+
+describe("Express.js port", () => {
+  const ORIGINAL_ENVS = process.env;
+  let testServer: Server;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...ORIGINAL_ENVS };
+  });
+
+  afterEach(async () => {
+    process.env = ORIGINAL_ENVS;
+    await testServer.close();
+  });
+
+  it.each([
+    {
+      testCase: "listen on any port if specified",
+      port: "5001",
+      expected: "Server is running on port 5001",
+    },
+    {
+      testCase: "listen on port 5000 if not specified",
+      port: undefined,
+      expected: "Server is running on port 5000",
+    },
+  ])("$testCase", async ({ testCase, port, expected }) => {
+    process.env.PORT = port;
+    const consoleLogSpyOn = jest.spyOn(console, "log");
+
+    // Import the server inside each test case after setting the PORT
+    jest.isolateModules(() => {
+      testServer = require("../index").server;
+    });
+
+    const res = await request(testServer).get("/api/quotes");
+    expect(process.env.PORT).toBe(port);
+    expect(res.statusCode).toBe(200);
+    expect(consoleLogSpyOn).toBeCalledWith(expected);
+    await testServer.close();
+  });
+});
+
 describe("GET /api/quotes", () => {
   it("should return all quotes", () => {
     return request(server)
@@ -70,25 +130,23 @@ describe("GET /api/quotes", () => {
       });
   });
   it("should cache response", async () => {
-    let res1 = await request(server)
-      .get("/api/quotes")
-    let res2 = await request(server)
-      .get("/api/quotes")
-    return expect(res2.body).toMatchObject(res1.body)
-  })
+    let res1 = await request(server).get("/api/quotes");
+    let res2 = await request(server).get("/api/quotes");
+    return expect(res2.body).toMatchObject(res1.body);
+  });
 });
 
 describe("quotesController", () => {
   it("should return 9 quotes", async () => {
-    let req = createRequest()
-    let res = createResponse()
-    await quotesController(req, res)
+    let req = createRequest();
+    let res = createResponse();
+    await quotesController(req, res);
     return expect(res._getJSONData()).toHaveLength(9);
-  })
-})
+  });
+});
 
 describe("getQuote", () => {
-  it("should return all quotes", () => {
+  it("should return all quotes if API call normal", () => {
     global.fetch = jest.fn(() =>
       Promise.resolve(new Response(JSON.stringify(MOCK_RES)))
     );
@@ -103,6 +161,27 @@ describe("getQuote", () => {
         "last_updated",
         "2018-08-09T21:56:28.000Z"
       );
+    });
+  });
+  it("should return empty value if API returns empty", () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve(new Response(JSON.stringify("")))
+    );
+    return getQuote(["BTC"], ["USD"]).then((result) => {
+      expect(result).toBe(null);
+    });
+  });
+  it("should return empty value if API returns 400", () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve(
+        new Response(JSON.stringify(MOCK_400_RES), {
+          status: 500,
+          statusText: "An internal server error occurred",
+        })
+      )
+    );
+    return getQuote(["BTC"], ["USD"]).then((result) => {
+      expect(result).toBe(null);
     });
   });
 });
